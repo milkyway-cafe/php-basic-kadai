e7e1ccd4967257afc42ba22421e9b2b7b3565314
milkyway-cafe <rainbowpastelflower@gmail.com>
2025-11-08 16:11:12 +0900

    Update foreach.php 訂正

--- kadai_011/foreach.php
+++ kadai_011/foreach.php
@@ -13,10 +13,11 @@
     $items = ['名前' => '玉ねぎ', '値段' => '200', '産地' => '北海道'];
 
     foreach ($items as $key => $value) {
-      echo "{$key} ; {$value}<br>";
+      echo "{$key} : {$value}<br>";
     }
 
     ?>
     </p>
   </body>
+
   </html>
