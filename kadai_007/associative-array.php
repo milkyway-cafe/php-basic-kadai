c79f1dcfdf80837fa1299d91ba1f40810544024a
milkyway-cafe <rainbowpastelflower@gmail.com>
2025-11-08 11:55:17 +0900

    kadai_007 アップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang = "ja">
+
+  <body>
+    <p>
+      <?php
+
+        $item = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+        print_r($item);
+      ?>
+    </p>
+  </body>
+</html>
