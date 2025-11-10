7860718251782c28754d37e08aa05d1bf3de62fc
milkyway-cafe <rainbowpastelflower@gmail.com>
2025-11-10 13:56:51 +0900

    kadai_sort アップロード

---
+++ kadai_sort/sort.php
@@ -0,0 +1,45 @@
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        
+        $nums = [15, 4, 18, 23, 10 ];
+
+        function sort_2way ($nums, $order) {
+
+          if ($order == true) {
+            
+            echo "昇順にソートします。<br>";
+            
+            sort($nums);
+          }
+
+          else {
+
+            echo "降順にソートします。<br>";
+
+            rsort($nums);
+            }
+
+          foreach ($nums as $value){
+          
+          echo "{$value}<br>";
+          };
+          };
+          
+          sort_2way($nums, true);
+          
+          sort_2way($nums, false);
+
+           
+        ?>
+    </p>
+</body>
+
+</html>
